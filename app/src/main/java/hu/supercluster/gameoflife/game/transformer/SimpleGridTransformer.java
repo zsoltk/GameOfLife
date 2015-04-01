3a0326f83a90a8b6fdda3468738164f23b04f079
zzss <zsolt.kocsi@gmail.com>
2015-04-01 02:39:07 +0200

    Fix SimpleGridTransformer working only when grid is square sized

--- app/src/main/java/hu/supercluster/gameoflife/game/transformer/SimpleGridTransformer.java
+++ app/src/main/java/hu/supercluster/gameoflife/game/transformer/SimpleGridTransformer.java
@@ -4,20 +4,18 @@ import hu.supercluster.gameoflife.game.cell.Cell;
 import hu.supercluster.gameoflife.game.grid.Grid;
 
 public class SimpleGridTransformer<T extends Cell> implements GridTransformer<T> {
-    int stateChanges[][];
-
     @Override
     public final void transform(Grid<T> grid, Rule<T> rule) {
-        stateChanges = new int[grid.getSizeY()][grid.getSizeX()];
+        int stateChanges[][] = new int[grid.getSizeY()][grid.getSizeX()];
 
         for (int j = 0; j < grid.getSizeY(); j++) {
-            for (int i = 0; i < grid.getSizeY(); i++) {
+            for (int i = 0; i < grid.getSizeX(); i++) {
                 stateChanges[j][i] = rule.apply(grid, i, j);
             }
         }
 
         for (int j = 0; j < grid.getSizeY(); j++) {
-            for (int i = 0; i < grid.getSizeY(); i++) {
+            for (int i = 0; i < grid.getSizeX(); i++) {
                 grid.getCell(i, j).setState(stateChanges[j][i]);
             }
         }
