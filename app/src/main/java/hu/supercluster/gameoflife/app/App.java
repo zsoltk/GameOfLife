e101fbc59836a1f7d67e4c16d43cf975b9f2d3e7
zzss <zsolt.kocsi@gmail.com>
2015-08-22 12:16:14 +0200

    Add Crashlytics

--- app/src/main/java/hu/supercluster/gameoflife/app/App.java
+++ app/src/main/java/hu/supercluster/gameoflife/app/App.java
@@ -2,5 +2,17 @@ package hu.supercluster.gameoflife.app;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+
 public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        initCrashlytics();
+    }
+
+    private void initCrashlytics() {
+        Crashlytics.start(this);
+    }
 }
