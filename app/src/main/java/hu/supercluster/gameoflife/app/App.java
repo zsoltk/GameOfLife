c7293d1e04f281ea3f301f9c8b967b818b14e026
zzss <zsolt.kocsi@gmail.com>
2015-08-22 12:53:36 +0200

    Add device name to Crashlytics

--- app/src/main/java/hu/supercluster/gameoflife/app/App.java
+++ app/src/main/java/hu/supercluster/gameoflife/app/App.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.crashlytics.android.Crashlytics;
 
 import hu.supercluster.gameoflife.BuildConfig;
+import tslamic.github.io.adn.DeviceNames;
 
 public class App extends Application {
     @Override
@@ -18,5 +19,6 @@ public class App extends Application {
         Crashlytics.start(this);
         Crashlytics.setString("Build time", BuildConfig.BUILD_TIME);
         Crashlytics.setString("Git SHA", BuildConfig.GIT_SHA);
+        Crashlytics.setString("Device model", DeviceNames.getCurrentDeviceName("Unknown"));
     }
 }
