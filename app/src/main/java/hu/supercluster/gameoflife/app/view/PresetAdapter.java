6da6b43297368695905bc660e0822fa59339f23e
Zsolt Kocsi <zsolt.kocsi@gmail.com>
2016-06-07 20:36:32 +0200

    Add new rules

--- app/src/main/java/hu/supercluster/gameoflife/app/view/PresetAdapter.java
+++ app/src/main/java/hu/supercluster/gameoflife/app/view/PresetAdapter.java
@@ -20,12 +20,17 @@ public class PresetAdapter extends ArrayAdapter<Preset> {
         final Resources resources = context.getResources();
         presets.add(new Preset(resources.getString(R.string.preset_pick_one), null));
         presets.add(new Preset(resources.getString(R.string.preset_life), "23/3"));
+        presets.add(new Preset(resources.getString(R.string.preset_high_life), "23/36"));
         presets.add(new Preset(resources.getString(R.string.preset_sponge_cells), "01234567/456"));
         presets.add(new Preset(resources.getString(R.string.preset_slowly_growing_fizz), "45/345"));
         presets.add(new Preset(resources.getString(R.string.preset_drills), "245678/3"));
         presets.add(new Preset(resources.getString(R.string.preset_condensation), "125678/367"));
         presets.add(new Preset(resources.getString(R.string.preset_seeds), "/2"));
         presets.add(new Preset(resources.getString(R.string.preset_spread), "012345678/3"));
+        presets.add(new Preset(resources.getString(R.string.preset_maze), "12345/3"));
+        presets.add(new Preset(resources.getString(R.string.preset_amoeba), "1358/357"));
+        presets.add(new Preset(resources.getString(R.string.preset_diamoeba), "5678/35678"));
+        presets.add(new Preset(resources.getString(R.string.preset_memory), "1357/1357"));
 
         return presets;
     }
